759c168760dfc19e757c0ea296c16007e72b3e24
sdjeetTuteckOfficial <shubhajeet.das@tuteck.com>
2024-03-15 12:44:04 +0530

    initial code

---
+++ src/Sidebar.jsx
@@ -0,0 +1,37 @@
+const Sidebar = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <aside>
+      <div className='description'>
+        You can drag these nodes to the pane on the right.
+      </div>
+      <div
+        className='dndnode input'
+        draggable
+        onDragStart={(event) => onDragStart(event, 'input')}
+      >
+        Input Node
+      </div>
+      <div
+        className='dndnode'
+        draggable
+        onDragStart={(event) => onDragStart(event, 'default')}
+      >
+        Default Node
+      </div>
+      <div
+        className='dndnode output'
+        draggable
+        onDragStart={(event) => onDragStart(event, 'output')}
+      >
+        Output Node
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
